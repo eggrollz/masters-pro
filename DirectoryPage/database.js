51971f417cb89438bb922c9ead9820f238277447
Quyen Ha <Quyen@quyens-mbp.marana.local>
2018-07-27 12:55:49 -0700

    creating new master's project repo from command line

---
+++ DirectoryPage/database.js
@@ -0,0 +1,132 @@
+var class2018 = [
+{
+"name":"Quyen Ha",
+"jobtitle":"GIS Technician",
+"employer":"Town of Marana",
+"joblocation":"Marana, AZ",
+"yearstarted":"2018",
+"description":"Currently a 2018 candidate in the UA MS GIST program...",
+"interests":"development, webmaps, sustainability",
+"email":"quyenttha@gmail.com",
+"phone":"(571)-243-8787",
+"website":"linkedin.com/in/quyentha"
+},
+
+{
+"name":"Laura Grantham",
+"jobtitle":"Archeologist",
+"employer":"Construction, Inc",
+"joblocation":"Marana, AZ",
+"yearstarted":"2018",
+"description":"Currently a 2018 candidate in the UA MS GIST program...",
+"interests":"archeology, opinion surveys, flamingos",
+"email":"laura@email.com",
+"phone":"(111)111-1111",
+"website":"linkedin.com/in/1111"
+},
+
+{
+"name":"Mohamed Mohamed",
+"jobtitle":"GIS Technician",
+"employer":"Sulphur Springs Valley Electric Cooperative",
+"joblocation":"Bisbee, AZ",
+"yearstarted:":"2018",
+"description":"Currently a 2018 candidate in the UA MS GIST program...",
+"interests":"surveying, engineering, avoiding shrimp",
+"email":"mohamed@email.com",
+"phone":"(222)222-2222",
+"website":"linkedin.com/in/2222"
+},
+
+{
+"name":"Christy Elzey",
+"jobtitle":"Systems Engineer",
+"employer":"Raytheon",
+"joblocation":"Tucson, AZ",
+"yearstarted:":"2018",
+"description":"Currently a 2018 candidate in the UA MS GIST program...",
+"interests":"Python, cool socks",
+"email":"christy@email.com",
+"phone":"(333)333-3333",
+"website":"linkedin.com/in/3333"
+},
+
+{
+"name":"Jon Cutts",
+"jobtitle":"Astronaut",
+"employer":"Raytheon",
+"joblocation":"Tucson, AZ",
+"yearstarted:":"2018",
+"description":"Currently a 2018 candidate in the UA MS GIST program...",
+"interests":"Business, VB, development",
+"email":"jon@email.com",
+"phone":"(444)444-4444",
+"website":"linkedin.com/in/4444"
+}
+]
+
+var class2017 = [
+{
+"name":"Student 2017",
+"jobtitle":"GIS Technician",
+"employer":"ASRC Federal",
+"joblocation":"Washington, DC",
+"yearstarted":"2017",
+"description":"Graduated in 2017 in the MS GIST program...",
+"interests":"development, webmaps, sustainability",
+"email":"student2017@gmail.com",
+"phone":"(571)-243-8787",
+"website":"linkedin.com/in/student2017"
+},
+
+{
+"name":"Second Student 2017",
+"jobtitle":"GIS Technician",
+"employer":"ASRC Federal",
+"joblocation":"Washington, DC",
+"yearstarted":"2017",
+"description":"Graduated in 2017 in the MS GIST program...",
+"interests":"development, webmaps, sustainability",
+"email":"student2017@gmail.com",
+"phone":"(571)-243-8787",
+"website":"linkedin.com/in/student2017"
+},
+{
+"name":"Mohamed Mohamed",
+"jobtitle":"GIS Technician",
+"employer":"Sulphur Springs Valley Electric Cooperative",
+"joblocation":"Bisbee, AZ",
+"yearstarted:":"2018",
+"description":"Currently a 2018 candidate in the UA MS GIST program...",
+"interests":"surveying, engineering, avoiding shrimp",
+"email":"mohamed@email.com",
+"phone":"(222)222-2222",
+"website":"linkedin.com/in/2222"
+},
+
+{
+"name":"Christy Elzey",
+"jobtitle":"Systems Engineer",
+"employer":"Raytheon",
+"joblocation":"Tucson, AZ",
+"yearstarted:":"2018",
+"description":"Currently a 2018 candidate in the UA MS GIST program...",
+"interests":"Python, cool socks",
+"email":"christy@email.com",
+"phone":"(333)333-3333",
+"website":"linkedin.com/in/3333"
+},
+
+{
+"name":"Jon Cutts",
+"jobtitle":"Astronaut",
+"employer":"Raytheon",
+"joblocation":"Tucson, AZ",
+"yearstarted:":"2018",
+"description":"Currently a 2018 candidate in the UA MS GIST program...",
+"interests":"Business, VB, development",
+"email":"jon@email.com",
+"phone":"(444)444-4444",
+"website":"linkedin.com/in/4444"
+}
+]
