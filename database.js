4471c58c5f8c484adb93f091d4ad092ac8ebb92a
Quyen Ha <quyen@dhcp-10-132-149-255.uawifi.arizona.edu>
2018-11-14 10:49:20 -0700

    Changing json formatting to geojson. Also changed routes in prof.html and index.html codes andcode as a result.

--- database.js
+++ database.js
@@ -1,68 +1,125 @@
 var classData = {
   cstaff: [ {
-    "name":"Dr. Chris Lukinbeal",
-    "jobtitle":"Professor and Director",
-    "yearstarted":"staff",
-    "description":"The Director of the program",
-    "interests":"development, webmaps, sustainability",
-    "email":"clukinbeal@email.arizona.edu",
-    "phone":" ",
-    "website":"linkedin.com/in/xxxx"
-    }
-  ],
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [125.6, 10.1]
+      },
+    "properties": {
+      "name": "Dr. Chris Lukinbeal",
+      "jobtitle":"Professor and Director",
+      "employer": "University of Arizona",
+      "joblocation": "Tucson, AZ",
+      "yearstarted":"staff",
+      "description":"The Director of the Master's of Science in GIS Technologies program. Additionally, he teaches undergraduate classes and specializes in film geography.",
+      "interests":"cinema, film, cultural geography, cartography, visual culture",
+      "email":"clukinbeal@email.arizona.edu",
+      "phone":" ",
+      "website":"www.linkedin.com/in/chris-lukinbeal-36505b62/",
+      },
+  }],
 
   c2018: [
     {
-    "name":"Quyen Ha",
-    "jobtitle":"GIS Technician",
-    "employer":"Town of Marana",
-    "joblocation":"Marana, AZ",
-    "yearstarted":"2018",
-    "description":"Currently a 2018 candidate in the UA MS GIST program...",
-    "interests":"development, webmaps, sustainability",
-    "email":"quyenttha@gmail.com",
-    "phone":"(571)-243-8787",
-    "website":"linkedin.com/in/quyentha"
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-77.441764, 38.892465] //coordinates are in [longitude, latitude]
+        },
+      "properties": {
+        "name":"Quyen Ha",
+        "jobtitle":"GIS Analyst & Technician",
+        "employer":"RDM International, Inc.",
+        "joblocation":"Chantilly, VA",
+        "yearstarted":"2018",
+        "description":"I will be a M.S. GIS Technologies candidate after this presentation. My passion lies within geospatial data communication, organization, and manipulation.",
+        "interests":"development, webmaps, sustainability",
+        "email":"quyenttha@gmail.com",
+        "phone":"(571)-243-8787",
+        "website":"www.linkedin.com/in/quyentha"
+        },
     },
 
     {
-    "name":"Laura Grantham",
-    "jobtitle":"Archeologist",
-    "employer":"Construction, Inc",
-    "joblocation":"Marana, AZ",
-    "yearstarted":"2018",
-    "description":"Currently a 2018 candidate in the UA MS GIST program...",
-    "interests":"archeology, opinion surveys, flamingos",
-    "email":"laura@email.com",
-    "phone":"(111)111-1111",
-    "website":"linkedin.com/in/1111"
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-77.441764, 38.892465]
+        },
+      "properties": {
+        "name":"Laura Grantham",
+        "jobtitle":"Archeologist",
+        "employer":"Construction, Inc",
+        "joblocation":"Marana, AZ",
+        "yearstarted":"2018",
+        "description":"Currently a 2018 candidate in the UA MS GIST program...",
+        "interests":"archeology, opinion surveys, flamingos",
+        "email":"laura@email.com",
+        "phone":"(111)111-1111",
+        "website":"linkedin.com/in/1111"
+      },
     },
 
     {
-    "name":"Mohamed Mohamed",
-    "jobtitle":"GIS Technician",
-    "employer":"Sulphur Springs Valley Electric Cooperative",
-    "joblocation":"Bisbee, AZ",
-    "yearstarted":"2018",
-    "description":"Currently a 2018 candidate in the UA MS GIST program...",
-    "interests":"surveying, engineering, avoiding shrimp",
-    "email":"mohamed@email.com",
-    "phone":"(222)222-2222",
-    "website":"linkedin.com/in/2222"
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-77.441764, 38.892465]
+        },
+      "properties": {
+        "name":"Mohamed Mohamed",
+        "jobtitle":"GIS Technician",
+        "employer":"Sulphur Springs Valley Electric Cooperative",
+        "joblocation":"Bisbee, AZ",
+        "yearstarted":"2018",
+        "description":"Currently a 2018 candidate in the UA MS GIST program...",
+        "interests":"surveying, engineering",
+        "email":"mohamed@email.com",
+        "phone":"(222)222-2222",
+        "website":"linkedin.com/in/2222"
+      },
     },
 
     {
-    "name":"Christy Elzey",
-    "jobtitle":"Systems Engineer",
-    "employer":"Raytheon",
-    "joblocation":"Tucson, AZ",
-    "yearstarted":"2018",
-    "description":"Currently a 2018 candidate in the UA MS GIST program...",
-    "interests":"Python, cool socks",
-    "email":"christy@email.com",
-    "phone":"(333)333-3333",
-    "website":"linkedin.com/in/3333"
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-77.441764, 38.892465]
+        },
+      "properties": {
+        "name":"Christy Elzey",
+        "jobtitle":"Systems Engineer",
+        "employer":"Raytheon",
+        "joblocation":"Tucson, AZ",
+        "yearstarted":"2018",
+        "description":"Enjoys learning new concepts in programming. Hikes in the desert around Tucson, and plays with my awesome dog.",
+        "interests":"Python, Scripting, Programming",
+        "email":"christyelzey@email.arizona.edu",
+        "phone":"(520)332-5552",
+        "website":"www.linkedin.com/in/christy-elzey-85018747/"
     },
+  },
+
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [-77.441764, 38.892465]
+      },
+    "properties": {
+      "name":"Jenna Smith",
+      "jobtitle":"GIS Intern",
+      "employer":"City of Tucson",
+      "joblocation":"Tucson, AZ",
+      "yearstarted":"2018",
+      "description":"",
+      "interests":"",
+      "email":"",
+      "phone":"",
+      "website":""
+    },
+  },
+
 
     {
     "name":"Jon Cutts",
@@ -117,6 +174,8 @@ var classData = {
     },
 
     {
+
+
     "name":"Christy Elzey",
     "jobtitle":"Systems Engineer",
     "employer":"Raytheon",
